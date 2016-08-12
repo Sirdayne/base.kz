219b7a5af656b1caa54b74fdc575d057c2938903
Yernar Mussin <mussinyernar@gmail.com>
2016-08-12 12:34:03 +0600

    changed header footer cabinet added mobile

---
+++ js/animation.js
@@ -0,0 +1,43 @@
+/*
+$(document).ready(function() {
+  
+    var ratingCheckbox = $('.header-navigation').children('.header-line');
+    
+    $('.post-start-magic').waypoint( function() {  
+        alert('start')
+    }, {
+        offset: '50%'
+    });
+});
+
+
+       $('.box_up')
+           .waypoint( function(dir) {
+             if ( dir === 'down')
+                $(this)
+                    .removeClass('fadeOutDown')
+                    .addClass('fadeInUp');   
+            else
+                $(this)
+                    .removeClass('fadeInUp')
+                    .addClass('fadeOutDown');  
+           
+    }, {
+        offset: '100%'
+    })
+       
+          .waypoint( function(dir) {
+            if ( dir === 'down')
+        $(this)
+            .removeClass('fadeInUp')
+            .addClass('fadeOutDown');   
+           else 
+               $(this)
+                .removeClass('fadeOutDown')
+                .addClass('fadeInUp')
+           
+    }, {
+        offset: -1
+    });
+    
+*/
