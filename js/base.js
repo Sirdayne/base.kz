b861738bd5a4d278c7288194569c7d72c7cbfe13
Yernar Mussin <mussinyernar@gmail.com>
2016-08-23 17:11:05 +0600

    added registration form

--- js/base.js
+++ js/base.js
@@ -12,15 +12,53 @@ $(document).ready(function(){
     });
     
     $('.header-nav-login-rating').click(function() {
-
         $('.header-nav-popup').fadeToggle('fast');
-        
     });
     
     $('.header-mob-login').click(function() {
-
         $('.header-mob-popup').fadeToggle('fast');
+    });
+    
+    
+    
+    /* REGISTRATION & AUTHORIZATION*/
+    
+    $('.header-nav-login').click(function() {
+
+        $('.reg-form').fadeIn('fast');
+        $('.reg-form-background').fadeIn('slow');
         
     });
     
+    $('.reg-form-cancel').click(function() {
+
+        $('.reg-form').fadeOut('fast');
+        $('.reg-form-background').fadeOut('slow');
+        
+    });
+    
+    $('.reg-form-background').click(function() {
+
+        $('.reg-form').fadeOut('fast');
+        $('.reg-form-background').fadeOut('slow');
+        
+    });
+    
+    $('#registration').click(function() {
+        
+        $('#authorization').removeClass('reg-active');
+        $('.reg-authorization').hide();
+        $('.reg-registration').show();
+        $('#registration').addClass('reg-active');
+        
+    });
+    
+    $('#authorization').click(function() {
+        
+        $('#registration').removeClass('reg-active');
+        $('.reg-registration').hide();
+        $('.reg-authorization').show();
+        $('#authorization').addClass('reg-active');
+        
+    });
 });
